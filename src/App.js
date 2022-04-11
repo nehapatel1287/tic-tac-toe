065bb69dc0983747b4215b73bd0ab87b9e496d24
Neha Patel <nehap.1287@gmail.com>
2022-04-11 12:55:15 +0530

    added all possibe winning possition

--- src/App.js
+++ src/App.js
@@ -17,16 +17,7 @@ export default function App() {
   ]);
   const [turn, setTurn] = React.useState("cross");
   const [winner, setWinner] = React.useState(undefined);
-  const winnerGrid = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+  const winnerGrid = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
   //no more turns or winnning position
 
   let changeGrid = (index) => {
